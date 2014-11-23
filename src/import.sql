9a9b52fe80e759700560087a1f39346cf75dad29
Arne Zeising <arne.zeising@gmail.com>
2014-11-24 00:17:53 +0100

    added entries to import.sql

--- src/import.sql
+++ src/import.sql
@@ -9,6 +9,7 @@ INSERT INTO century (name, cohort_id, number_of_students, break_time) VALUES ('W
 INSERT INTO century (name, cohort_id, number_of_students, break_time) VALUES ('W12b', 3, 28, 18);
 INSERT INTO lecturer (name, break_time) VALUES ('Hinrich Schröder', 25);
 INSERT INTO lecturer (name, break_time) VALUES ('Stephan Anft', 45);
+INSERT INTO lecturer (name, break_time) VALUES ('Stefan Reichert', 45);
 INSERT INTO room (name, break_time, available_seats) VALUES ('A001', 30, 25);
 INSERT INTO room (name, break_time, available_seats) VALUES ('C101', 15, 30);
 INSERT INTO course (name, break_time, lecturer_id,cohort_id,century_id) VALUES ('Controlling', 15, 1,2,null);
@@ -22,6 +23,8 @@ INSERT INTO lesson (start_date,end_date,course_id) VALUES ('2014-11-10 9:15:00',
 INSERT INTO lesson (start_date,end_date,course_id) VALUES ('2014-11-11 11:30:00','2014-11-11 13:20:00',2);
 INSERT INTO lesson (start_date,end_date,course_id) VALUES ('2014-11-12 12:50:00','2014-11-12 14:45:00',2);
 INSERT INTO lesson (start_date,end_date,course_id) VALUES ('2014-11-13 9:40:00','2014-11-13 10:30:00',3);
+INSERT INTO lesson (start_date,end_date,course_id) VALUES ('2014-11-24 10:40:00','2014-11-24 12:30:00',3);
+INSERT INTO lesson (start_date,end_date,course_id) VALUES ('2014-11-28 9:40:00','2014-11-28 10:30:00',3);
 INSERT INTO lesson_room (lesson_id,room_id) VALUES (1,1);
 INSERT INTO lesson_room (lesson_id,room_id) VALUES (1,2);
 INSERT INTO lesson_room (lesson_id,room_id) VALUES (2,2);
