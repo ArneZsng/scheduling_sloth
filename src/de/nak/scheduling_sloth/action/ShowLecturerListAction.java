78550f63c00833c025a753eb4194095ba49076e7
HndrkMkt <hendrik.makait@nordakademie.de>
2014-11-23 23:44:50 +0100

    Fixed actionErrors.

--- src/de/nak/scheduling_sloth/action/ShowLecturerListAction.java
+++ src/de/nak/scheduling_sloth/action/ShowLecturerListAction.java
@@ -1,16 +1,17 @@
 package de.nak.scheduling_sloth.action;
 
-import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.Preparable;
 import de.nak.scheduling_sloth.exception.EntityNotFoundException;
 import de.nak.scheduling_sloth.model.Lecturer;
 import de.nak.scheduling_sloth.service.LecturerService;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 
 /**
  * Created by arne on 10/28/14.
  */
-public class ShowLecturerListAction extends AbstractAction implements Action{
+public class ShowLecturerListAction extends AbstractAction implements Preparable{
     /** The list of lecturers. */
     private List<Lecturer> lecturerList;
 
@@ -18,13 +19,11 @@ public class ShowLecturerListAction extends AbstractAction implements Action{
     private LecturerService lecturerService;
 
     @Override
-    public String execute() {
+    public void prepare() {
         try {
             lecturerList = lecturerService.loadAllLecturers();
-            return SUCCESS;
         } catch (EntityNotFoundException e) {
             addActionError(getText(e.getMessage()));
-            return ERROR;
         }
     }
 
